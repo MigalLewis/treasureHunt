7dc0aff421139f9e73a2e1744e5126467662c876
MigalLewis <migallewis@gmail.com>
2026-01-30 12:01:47 +0200

    Add separate QR entry page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { QrEntryComponent } from './qr-entry/qr-entry.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'qr-entry',
+    component: QrEntryComponent
+  },
+  {
+    path: 'qr-entry/:code',
+    component: QrEntryComponent
+  }
+];
