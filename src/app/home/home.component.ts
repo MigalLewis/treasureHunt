2c4ecf2e82b6cc63c788d98cf44ff6097fd493a0
MigalLewis <migallewis@gmail.com>
2026-01-31 09:09:33 +0200

    Add Parktown North hero illustration

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,43 @@
+import { Component, Signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { RouterLink } from '@angular/router';
+
+import {
+  TreasureDataService,
+  fallbackHomeContent
+} from '../services/treasure-data.service';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+  title = 'Neighborhood Treasure Hunt';
+
+  readonly homeContent: Signal<typeof fallbackHomeContent>;
+
+  constructor(private dataService: TreasureDataService) {
+    this.homeContent = toSignal(this.dataService.getHomeContent$(), {
+      initialValue: fallbackHomeContent
+    });
+  }
+
+  get huntSteps() {
+    return this.homeContent().huntSteps;
+  }
+
+  get businessStops() {
+    return this.homeContent().businessStops;
+  }
+
+  get ticketTiers() {
+    return this.homeContent().ticketTiers;
+  }
+
+  get faqItems() {
+    return this.homeContent().faqItems;
+  }
+}
